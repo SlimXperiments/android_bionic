1977c1f044f7856c23506863ac82cb8853140b86
kszaq <kszaquitto@gmail.com>
2013-12-18 23:16:27 +0100

    libc: Unbreak build for scorpion
    Use cortex-a15 memcpy and krait memset routines
    
    Change-Id: I02cb178b33b01fc91e903f1f7b620640fd0f3310
    
    Conflicts:
    	libc/arch-arm/scorpion/scorpion.mk

--- libc/arch-arm/scorpion/scorpion.mk
+++ libc/arch-arm/scorpion/scorpion.mk
@@ -1,6 +1,10 @@
 libc_bionic_src_files_arm += \
-    arch-arm/scorpion/bionic/memcpy.S \
-    arch-arm/scorpion/bionic/memset.S \
+    arch-arm/cortex-a15/bionic/memcpy.S \
+    arch-arm/krait/bionic/memset.S \
     arch-arm/krait/bionic/strcmp.S \
     arch-arm/krait/bionic/memmove.S \
     arch-arm/cortex-a15/bionic/strlen.S \
+    arch-arm/cortex-a15/bionic/strcat.S \
+    arch-arm/cortex-a15/bionic/strcpy.S \
+    arch-arm/cortex-a15/bionic/__strcat_chk.S \
+    arch-arm/cortex-a15/bionic/__strcpy_chk.S \
